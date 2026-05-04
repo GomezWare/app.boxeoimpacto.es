c32e29cafff85776bbc4437932f7335e7126ec4a
David Gómez <streetninjacoder@gmail.com>
2026-05-04 23:58:56 +0200

    implementar configuracion csrf

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,7 +1,15 @@
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv';
+dotenv.config({ quiet: true });
 
+
+// COnfiguración anti CSRF para que solo se puedan hacer peticiones desde los hosts permitidos
+const ALLOWED_HOSTS = process.env.ALLOWED_HOSTS ?? '';
+if (!ALLOWED_HOSTS) throw new Error('ALLOWED_HOSTS is not set');
+const ALLOWED_HOSTS_ARRAY = ALLOWED_HOSTS.split(',');
 
 export default defineNuxtConfig({
+
   compatibilityDate: '2025-07-15',
   
   devtools: { enabled: true },
@@ -12,7 +20,11 @@ export default defineNuxtConfig({
 
   ssr: false,
 
+
   vite: {
+    server: {
+      allowedHosts: ALLOWED_HOSTS_ARRAY,
+    },
     plugins: [tailwindcss()],
     optimizeDeps: {
       include: ['@vue/devtools-core', '@vue/devtools-kit'],
